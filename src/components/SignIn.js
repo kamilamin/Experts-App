e038462a77cfd6665a5f8701900119fe8045128f
kamil amin <Kamil.amin@live.co.uk>
2017-10-17 00:05:03 +0500

    Update UI of SignIn component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { firebaseApp } from '../firebase';
+import signup from './image/cancer_patient.jpg';
 
 const form_heading = {
     textAlign: 'center',
@@ -9,8 +10,9 @@ const form_heading = {
 
 const form_login = {
     border: '1px solid #eee',
-    padding: '5%',
-    marginLeft: '70%',
+    padding: '4%',
+    marginLeft: '60%',
+    marginTop: '-24%',
     boxShadow: '10px 10px 50px #eee'
 };
 
@@ -37,8 +39,9 @@ class SignIn extends Component{
         return (
             <div style={{margin: '5%'}}>
                 <div>
-                    <h1>Expert System For Dermatologist</h1>
+                    <h1 style={{marginBottom: '5%', textAlign: 'center'}}>Expert System For Dermatologist</h1>
                 </div>
+                <img style={{width: '55%', height: '80%'}} src={signup} alt='Welcome to system' />
                 <div style={form_login}>
                     <h2 style={form_heading}>SIGN IN</h2>
                     <input className="form-control" type="text" placeholder='Email Address' onChange={event => this.setState({email: event.target.value})} style={{marginBottom: '5px'}} required />
